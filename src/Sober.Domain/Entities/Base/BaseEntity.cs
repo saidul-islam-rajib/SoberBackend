93a2b0e717c5c7e2cd8a4f4dab935f60317d8eb6
Rajib <saidul.bd@asa-international.com>
2023-06-23 16:18:52 +0600

    Add base entity class in entity folder

---
+++ src/Sober.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Sober.Domain.Entities.Base
+{
+    public class BaseEntity <TKey> where TKey : struct
+    {
+        public TKey Id { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+        public string AuthorizeStatus { get; set; } = "U";
+        public Guid? AuthorizedBy { get; set; }
+        public DateTime? AuthorizedDate { get; set; }
+        public Guid? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
