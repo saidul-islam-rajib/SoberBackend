1dd07558b941534da2164f6a29a3c773b1c68d19
Rajib <saidul.bd@asa-international.com>
2023-06-24 00:05:00 +0600

    Add comment entity

---
+++ src/Sober.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,14 @@
+﻿using Sober.Domain.Entities.Base;
+
+namespace Sober.Domain.Entities
+{
+    public class CommentEntity : BaseEntity<Guid>
+    {
+        public int CommentId { get; set; }
+        public string UserName { get; set; }
+        public DateTime CommentDate { get; set; }
+        public string UserComment { get; set; }
+        public string UserPicture { get; set; }
+
+    }
+}
